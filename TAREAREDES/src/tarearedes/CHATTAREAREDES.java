455c3bb0569eae5cb9d83177e70570a2eb9e3a73
Luis Rojas <luis_felipe_rojas_c@hotmail.com>
2018-09-26 09:24:25 -0300

    Danko
    tarearedes

---
+++ TAREAREDES/src/tarearedes/CHATTAREAREDES.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarearedes;
+
+/**
+ *
+ * @author danko
+ */
+public class CHATTAREAREDES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Servidor sas= new Servidor();
+        sas.dispose();
+        Cliente cl = new Cliente();
+    }
+    
+}
